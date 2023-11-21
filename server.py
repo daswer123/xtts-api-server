c036987dae41f3196295b54e5ec0109abbd9a65b
Danil Boldyrev <daswerq123@gmail.com>
2023-11-21 06:39:39 +0300

    Work on the server code is finished

--- server.py
+++ server.py
@@ -1,81 +1,137 @@
-import torch
 from TTS.api import TTS
 from fastapi import FastAPI, HTTPException
-from pydantic import BaseModel
+from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import FileResponse
+from fastapi.responses import StreamingResponse
+
+from pydantic import BaseModel
 import uvicorn
+
+import shutil
 from loguru import logger
 from argparse import ArgumentParser
 
+from tts_funcs import TTSWrapper
+
+# Default Folders , you can change them via API
+OUTPUT_FOLDER = "output"
+SPEAKER_FOLDER = "speakers"
+
+# Create an instance of the TTSWrapper class and server
 app = FastAPI()
+XTTS = TTSWrapper()
 
-# Load model once at server startup
-logger.info("Model initialization.")
-device = "cuda" if torch.cuda.is_available() else "cpu"
-tts_model = TTS("tts_models/multilingual/multi-dataset/xtts_v2").to(device)
+# Add CORS middleware 
+origins = ["*"]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
-# List of supported language codes
-supported_languages = {
-    "ar":"Arabic",
-    "pt":"Brazilian Portuguese",
-    "zh-cn":"Chinese",
-    "cs":"Czech",
-    "nl":"Dutch",
-    "en":"English",
-    "fr":"French",
-    "de":"German",
-    "it":"Italian",
-    "pl":"Polish",
-    "ru":"Russian",
-    "es":"Spanish",
-    "tr":"Turkish",
-    "ja":"Japanese",
-    "ko":"Korean",
-    "hu":"Hungarian"
-}
+class OutputFolderRequest(BaseModel):
+    output_folder: str
 
-@app.get("/languages/")
-def get_languages():
-    return supported_languages
+class SpeakerFolderRequest(BaseModel):
+    speaker_folder: str
 
 class SynthesisRequest(BaseModel):
     text: str
-    speaker_wav: str #
+    speaker_wav: str 
     language: str
 
 class SynthesisFileRequest(BaseModel):
     text: str
-    speaker_wav: str
+    speaker_wav: str 
     language: str
-    output_path: str # The user specifies the path to save the file
+    output_path: str  
+
+@app.get("/speakers/")
+def get_speakers():
+    speakers = XTTS.get_speakers()
+    return {"speakers": speakers}
+
+@app.get("/languages/")
+def get_languages():
+    languages = XTTS.list_languages()
+    return {"languages": languages}
+
+@app.get("/get_folders/")
+def get_folders():
+    speaker_folder = XTTS.speaker_folder
+    output_folder = XTTS.output_folder
+    return {"speaker_folder": speaker_folder, "output_folder": output_folder}
+
+@app.post("/set_output/")
+def set_output(output_req: OutputFolderRequest):
+    try:
+        XTTS.set_out_folder(output_req.output_folder)
+        return {"message": f"Output folder set to {output_req.output_folder}"}
+    except ValueError as e:
+        raise HTTPException(status_code=400, detail=str(e))
+
+@app.post("/set_speaker_folder/")
+def set_speaker_folder(speaker_req: SpeakerFolderRequest):
+    try:
+        XTTS.set_speaker_folder(speaker_req.speaker_folder)
+        return {"message": f"Speaker folder set to {speaker_req.speaker_folder}"}
+    except ValueError as e:
+        raise HTTPException(status_code=400, detail=str(e))
 
 @app.post("/tts_to_audio/")
-def synthesize_to_audio(request: SynthesisRequest):
+async def tts_to_audio(request: SynthesisRequest):
     try:
-        # Generating a temporary file to be transferred to the user.
-        temporary_file_path = "temp_output.wav"
-        tts_model.tts_to_file(
+        # Validate language code against supported languages.
+        if request.language.lower() not in supported_languages:
+            raise HTTPException(status_code=400,
+                                detail="Language code sent is either unsupported or misspelled.")
+
+        # Generate an audio file using process_tts_to_file.
+        output_file_path = XTTS.process_tts_to_file(
             text=request.text,
-            speaker_wav=request.speaker_wav,
-            language=request.language,
-            output_path=temporary_file_path
+            speaker_name_or_path=request.speaker_wav,
+            language=request.language.lower(),
+            file_name_or_path=None  # Generate a unique temp filename within the method.
         )
-        return FileResponse(path=temporary_file_path, media_type='audio/wav', filename="output.wav")
+
+        def iterfile():
+            with open(output_file_path, mode="rb") as file_like:  # read the file as stream
+                yield from file_like
+
+        response = StreamingResponse(iterfile(), media_type='audio/wav')
+
+        # Set content disposition header to prompt download with proper filename
+        response.headers["Content-Disposition"] = "attachment; filename=output.wav"
+
+        return response
+
     except Exception as e:
-        raise HTTPException(status_code=500, detail=str(e))
+        raise HTTPException(status_code=500, detail=f"An eldritch error has occurred: {str(e)}")
+
 
 @app.post("/tts_to_file/")
-def synthesize_to_file(request: SynthesisFileRequest):
+async def tts_to_file(request: SynthesisFileRequest):
     try:
-        tts_model.tts_to_file(
+        logger.info(f"Processing TTS to file with request: {request}")
+
+        # Validate language code against supported languages.
+        if request.language.lower() not in supported_languages:
+             raise HTTPException(status_code=400,
+                                 detail="Language code sent is either unsupported or misspelled.")
+
+        # Now use process_tts_to_file for saving the file.
+        output_file = XTTS.process_tts_to_file(
             text=request.text,
-            speaker_wav=request.speaker_wav,
-            language=request.language,
-            output_path=request.file_path  # The user-provided path to save the file is used here.
+            speaker_name_or_path=request.speaker_wav,
+            language=request.language.lower(),
+            file_name_or_path=request.file_path  # The user-provided path to save the file is used here.
         )
-        return {"message": "The file was successfully saved", "output_path": request.file_path}
+        return {"message": "The scroll of sound has been sealed and saved.", "output_path": output_file}
+
     except Exception as e:
-        raise HTTPException(status_code=500, detail=str(e))
+         raise HTTPException(status_code=500, detail=f"A sinister fault has slipped through: {str(e)}")
 
 if __name__ == "__main__":
     parser = ArgumentParser(description="Run the Uvicorn server.")
@@ -83,6 +139,6 @@ if __name__ == "__main__":
     parser.add_argument("-p", "--port", default=8020, type=int, help="Port to bind")
 
     args = parser.parse_args()
+    logger.info("Starting server...")
 
-    logger.info("Server ready.")
     uvicorn.run(app, host=args.host, port=args.port)
---
+++ tts_funcs.py
@@ -0,0 +1,123 @@
+# tts.py
+
+import torch
+from TTS.api import TTS
+from loguru import logger
+import os
+
+# List of supported language codes
+supported_languages = {
+    "ar":"Arabic",
+    "pt":"Brazilian Portuguese",
+    "zh-cn":"Chinese",
+    "cs":"Czech",
+    "nl":"Dutch",
+    "en":"English",
+    "fr":"French",
+    "de":"German",
+    "it":"Italian",
+    "pl":"Polish",
+    "ru":"Russian",
+    "es":"Spanish",
+    "tr":"Turkish",
+    "ja":"Japanese",
+    "ko":"Korean",
+    "hu":"Hungarian"
+}
+
+reversed_supported_languages = {name: code for code, name in supported_languages.items()}
+
+class TTSWrapper:
+    def __init__(self,output_folder = "./output", speaker_folder="./speakers"):
+        self.device = "cuda" if torch.cuda.is_available() else "cpu"
+        self.model = TTS("tts_models/multilingual/multi-dataset/xtts_v2").to(self.device)
+        self.speaker_folder = speaker_folder
+        self.output_folder = output_folder
+
+        self.create_directories()
+
+    def create_directories(self):
+        # A list of all mystical places to be checked or conjured.
+        directories = [self.output_folder, self.speaker_folder]
+
+        for sanctuary in directories:
+            # Ensure the path is absolute and normalized to prevent any sorcery!
+            absolute_path = os.path.abspath(os.path.normpath(sanctuary))
+
+            if not os.path.exists(absolute_path):
+                # If it does not exist in this dimension, then we must bring it into being!
+                os.makedirs(absolute_path)
+                print(f"The sacred space at {absolute_path} has been manifested!")
+            else:
+                # If it exists already, let us acknowledge its ancient presence.
+                print(f"The venerable domain at {absolute_path} stands firm.")
+
+    def set_speaker_folder(self, folder):
+        if os.path.exists(folder) and os.path.isdir(folder):
+            self.speaker_folder = folder
+            create_directories(self)
+            logger.info(f"Speaker folder is set to {folder}")
+        else:
+            raise ValueError("Provided path is not a valid directory")
+
+    def set_out_folder(self, folder):
+        if os.path.exists(folder) and os.path.isdir(folder):
+            self.output_folder = folder
+            create_directories(self)
+            logger.info(f"Output folder is set to {folder}")
+        else:
+            raise ValueError("Provided path is not a valid directory")
+
+    def list_speakers(self):
+        speakers_list = [f for f in os.listdir(self.speaker_folder) if f.endswith('.wav')]
+        return speakers_list
+
+    def get_speakers(self):
+        # Use os.path.splitext to split off the extension and take only the name
+        speakers_list = [os.path.splitext(f)[0] for f in os.listdir(self.speaker_folder) if f.endswith('.wav')]
+        return speakers_list
+    
+    def list_languages(self):
+        return reversed_supported_languages
+
+    def process_tts_to_file(self, text, speaker_name_or_path, language, file_name_or_path="out.wav"):
+        try:
+            # Check if the speaker path is a .wav file or just the name
+            if speaker_name_or_path.endswith('.wav'):
+                if os.path.isabs(speaker_name_or_path):
+                    # If it's an absolute path for the speaker file
+                    speaker_wav = speaker_name_or_path
+                else:
+                    # It's just a filename; append it to the speakers folder
+                    speaker_wav = os.path.join(self.speaker_folder, speaker_name_or_path)
+            else:
+                # Look for the corresponding .wav in our list of speakers
+                speakers_list = self.list_speakers()
+                if f"{speaker_name_or_path}.wav" in speakers_list:
+                    speaker_wav = os.path.join(self.speaker_folder, f"{speaker_name_or_path}.wav")
+                else:
+                    raise ValueError(f"Speaker {speaker_name_or_path} not found.")
+
+            # Determine output path based on whether a full path or a file name was provided
+            if os.path.isabs(file_name_or_path):
+                # An absolute path was provided by user; use as is.
+                output_file = file_name_or_path
+            else:
+                # Only a filename was provided; prepend with output folder.
+                output_file = os.path.join(self.output_folder, file_name_or_path)
+
+            self.model.tts_to_file(
+                text=text,
+                speaker_wav=speaker_wav,
+                language=language,
+                file_path=output_file  # Assuming tts_to_file takes 'file_path' as an argument.
+            )
+
+            return output_file
+
+        except Exception as e:
+            raise e  # Propagate exceptions for endpoint handling.
+
+
+
+        
